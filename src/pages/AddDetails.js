f1cdfd3e9fbca58d97f43414f62b946b620cc1f9
benedict <benedict@qburst.com>
2025-04-09 11:54:30 +0530

    added flight details and rooms to add details page

--- src/pages/AddDetails.js
+++ src/pages/AddDetails.js
@@ -4,17 +4,29 @@ import "react-datepicker/dist/react-datepicker.css";
 import { formatDateToDDMMYYYY } from "../helper";
 import AddPassenger from "../components/AddPassenger";
 import { useLocation, useNavigate } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../redux/store";
+import { setFlightDetails, setRooms } from "../redux/createPdfSlice";
 
 function AddDetails() {
-  //   const [selectedForm, setSelectedForm] = useState(null);
   const [selectedStartDate, setSelectedStartDate] = useState(new Date());
   const [selectedEndDate, setSelectedEndDate] = useState(new Date());
   const [confirmationNumber, setConfirmationNumber] = useState("");
   const [passengerList, setPassengerList] = useState([]);
+  const flightDetails = useAppSelector((state) => state.createPdf.flights);
+  const error = useAppSelector((state) => state.createPdf.errors.flights);
+  const rooms = useAppSelector((state) => state.createPdf.rooms);
+  const dispatch = useAppDispatch();
 
   const location = useLocation();
   const { selectedForm } = location.state || {};
-
+  const {
+    arrivalCity,
+    arrivalFlightNumber,
+    arrivalTime,
+    departureCity,
+    departureFlightNumber,
+    departureTime,
+  } = flightDetails || {};
   const navigate = useNavigate();
 
   const handleAddDays = (date) => {
@@ -37,6 +49,7 @@ function AddDetails() {
         confirmationNumber,
         passengerList,
         selectedForm,
+        flights: flightDetails,
         selectedStartDate,
         selectedEndDate,
       },
@@ -94,6 +107,145 @@ function AddDetails() {
             disabled
             type="text"
           />
+        </div>
+        <div className="my-4">
+          <h1 className="mb-2 text-start">Rooms</h1>
+          <input
+            type="number"
+            id={`rooms}`}
+            name={`rooms}`}
+            value={rooms}
+            onChange={(e) => dispatch(setRooms(e.target.value))}
+            className="w-full px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring focus:ring-blue-300"
+          />
+        </div>
+        <div className="">
+          {/* <h1 className="text-2xl text-start font-bold mb-6">Flight Details</h1> */}
+          <div className="flex-col">
+            <div className="mt-5 flex flex-col justify-start">
+              <h1 className="mb-2 text-start">Arrival City</h1>
+              <input
+                type="text"
+                id={`arrival-city`}
+                name={`ArrivalCity`}
+                value={arrivalCity}
+                onChange={(e) =>
+                  dispatch(
+                    setFlightDetails({
+                      ...flightDetails,
+                      arrivalCity: e.target.value,
+                    })
+                  )
+                }
+                className="text-input"
+              />
+              <div className="text-red-500 mb-5">{error.arrivalCity}</div>
+            </div>
+
+            <div className="mt-5 flex flex-col justify-start">
+              <h1 className="mb-2 text-start">Arrival Flight Number</h1>
+              <input
+                type="text"
+                id={`flight-number`}
+                name={`FlightNumber`}
+                value={arrivalFlightNumber}
+                onChange={(e) =>
+                  dispatch(
+                    setFlightDetails({
+                      ...flightDetails,
+                      arrivalFlightNumber: e.target.value,
+                    })
+                  )
+                }
+                className="text-input"
+              />
+              <div className="text-red-500 mb-5">
+                {error.arrivalFlightNumber}
+              </div>
+            </div>
+
+            <div className="mt-5 flex flex-col justify-start">
+              <h1 className="mb-2 text-start">Arrival Time</h1>
+              <input
+                type="text"
+                id={`arrival-time`}
+                name={`ArrivalTime`}
+                value={arrivalTime}
+                onChange={(e) =>
+                  dispatch(
+                    setFlightDetails({
+                      ...flightDetails,
+                      arrivalTime: e.target.value,
+                    })
+                  )
+                }
+                className="text-input"
+              />
+              <div className="text-red-500 mb-5">{error.arrivalTime}</div>
+            </div>
+          </div>
+
+          <div className="mt-5 flex flex-col justify-start">
+            <h1 className="mb-2 text-start">Departure City</h1>
+            <input
+              type="text"
+              id={`departure-city`}
+              name={`DepartureCity`}
+              value={departureCity}
+              onChange={(e) =>
+                dispatch(
+                  setFlightDetails({
+                    ...flightDetails,
+                    departureCity: e.target.value,
+                  })
+                )
+              }
+              className="text-input"
+            />
+            <div className="text-red-500 mb-5 mt-2">{error.departureCity}</div>
+          </div>
+
+          <div className="mt-5 flex flex-col justify-start">
+            <h1 className="mb-2 text-start">Departure Flight Number</h1>
+            <input
+              type="text"
+              id={`departure-flight`}
+              name={`DepartureFlight`}
+              value={departureFlightNumber}
+              onChange={(e) =>
+                dispatch(
+                  setFlightDetails({
+                    ...flightDetails,
+                    departureFlightNumber: e.target.value,
+                  })
+                )
+              }
+              className="text-input"
+            />
+            <div className="text-red-500 mt-2">
+              {error.departureFlightNumber}
+            </div>
+          </div>
+
+          <div className="mt-5 flex flex-col justify-start">
+            <h1 className="mb-2 text-start">Departure Time</h1>
+            <input
+              type="text"
+              id={`departure-time`}
+              name={`DepartureTime`}
+              value={departureTime}
+              onChange={(e) =>
+                dispatch(
+                  setFlightDetails({
+                    ...flightDetails,
+                    departureTime: e.target.value,
+                  })
+                )
+              }
+              className="text-input"
+            />
+            <div className="text-red-500 mt-2">{error.departureTime}</div>
+          </div>
         </div>
         <button
           onClick={onSavePressed}
